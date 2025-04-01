e5c4852c814eb0baf66c9b06678ad72ed072d951
haihuynh2002 <info.haihuynh@gmail.com>
2025-04-01 17:16:30 +0700

    fix: test

--- src/test/java/com/polarbookshop/catalog_service/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalog_service/CatalogServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.polarbookshop.catalog_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("integration")
 class CatalogServiceApplicationTests {
 
 	@Test
